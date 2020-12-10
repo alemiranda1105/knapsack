15d4a9b69f3d45c7a609c484a655733559bf8065
alemiranda1105 <amiranda110500@gmail.com>
2020-12-10 13:12:44 +0100

    Argumentos añadidos

--- Knapsack.py
+++ Knapsack.py
@@ -14,20 +14,25 @@ def start():
 
 def args_creator():
     parser = ArgumentParser()
-    parser.add_argument("-d", "--directory", help="directory (process many files)", metavar="[DIRECTORY]")
-    parser.add_argument("-f", "--file", help="file (process a single file)", metavar="[FILE]")
-    parser.add_argument("-b", "--benefit", help="Display benefit")
-    parser.add_argument("-r", "--room", help="Display room (unused knapsack weight)")
-    parser.add_argument("-t", "--time", help="Display time")
-    parser.add_argument("-dt", "--display_taken", help="Display identifier of taken items")
-    parser.add_argument("-sg", "--greedy", nargs='?', help="Solve it with Greedy")
-    parser.add_argument("-sm", "--memoization", nargs='?', help="Solve it with Memoization")
-    parser.add_argument("-st", "--tabulation", nargs='?', help="Solve it with Tabulation")
+    parser.add_argument("-d", "--directory", action="store", dest="directory", default="", help="directory (process many files)", metavar="[DIRECTORY]")
+    parser.add_argument("-f", "--file", action="store", dest="file", default="", help="file (process a single file)", metavar="[FILE]")
+    parser.add_argument("-b", "--benefit", action="store_true", dest="showBenefit", default=False, help="Display benefit")
+    parser.add_argument("-r", "--room", action="store_true", dest="showRoom", default=False, help="Display room (unused knapsack weight)")
+    parser.add_argument("-t", "--time", action="store_true", dest="timer", default=False, help="Display time")
+    parser.add_argument("-dt", "--display_taken", action="store_true", dest="showTaken", default=False, help="Display identifier of taken items")
+    parser.add_argument("-sg", "--greedy", action="store_true", dest="greedy", default=False, help="Solve it with Greedy")
+    parser.add_argument("-sm", "--memoization", action="store_true", dest="memoization", default=False, help="Solve it with Memoization")
+    parser.add_argument("-st", "--tabulation", action="store_true", dest="tabulation", default=False, help="Solve it with Tabulation")
 
-    parser.parse_args()
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    start()
+    args = args_creator()
+    print(args.directory)
+    if args.timer:
+        print("timer")
+    else:
+        print("casi")
 
 
